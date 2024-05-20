c0525e098beb9f4239bea3589badf3ec8a03145c
felipeclarindo <fglpc2@gmail.com>
2024-05-20 03:39:52 -0300

    Adicionando e corrigindo validações

--- src/utils/validations.py
+++ src/utils/validations.py
@@ -2,8 +2,9 @@ import datetime
 
 def validarSenha(senha:str) -> bool:
     try:
+        senha = senha.strip()
         if len(senha) == 0:
-            raise ValueError("A Senha ão pode ser vazia!")
+            raise ValueError("A Senha não pode ser vazia!")
         elif len(senha) >= 15:
             numericos = 0
             maiusculo = 0
@@ -25,87 +26,242 @@ def validarSenha(senha:str) -> bool:
             if numericos >= 2 and maiusculo >= 2 and minusculo >= 2 and especiais >= 2:
                 return True
         else:
-            raise ValueError("Senha invalida!")
-        return False
+            raise Exception("Senha invalida!")
+    except ValueError:
+        print("Valor invalido!")
     except Exception as e:
         print(e)
+    return False
+
+
+def validarEmail(email:str) -> bool:
+    try:
+        email = email.strip()
+        if len(email) > 0:
+            if "@" in email:
+                email_separado = email.split("@")
+                if len(email_separado) == 2:
+                    dominio = email_separado[1]
+                    if "." in dominio:
+                        dominio_separado = dominio.split(".")
+                        if len(dominio_separado) in [2, 3]:
+                            for dominio_parte in dominio_separado:
+                                if len(dominio_parte) < 2 or len(email_separado[0]) < 3:
+                                    raise Exception ("Email invalido!")
+                                else:
+                                    return True
+                        else:
+                            raise Exception("Email Invalido")
+                    else:
+                        raise Exception("Email Invalido")
+                else:
+                    raise Exception("Email invalido!")
+            else:
+                raise Exception("É necessario ter @ no email!")
+        else:
+            raise Exception("O email não pode ser vazio!")
+    except ValueError:
+        print("Valor invalido")
+    except Exception as e:
+        print(e)
+    return False
+
+def validarUsuario(usuario:str) -> bool:
+    try:
+        usuario = usuario.strip()
+        if len(usuario) > 3:
+            usuario = usuario.replace("_", "")
+
+            if usuario.isalnum():
+                return True
+            else:
+                raise ValueError("Usuario pode conter apenas numeros letras e _")
+        else:
+            raise ValueError("O valor do usuario não pode ser menor que 3")
+    except Exception as e:
+        print(e)
+    return False
+
+def validarCpf(cpf:str) -> bool:
+    try:
+        cpf = cpf.strip()
+        if len(cpf) > 0:
+            if ".-" in cpf:
+                cpf = cpf.replace("-", "")
+                cpf = cpf.replace(".", "")
+
+            if cpf.isdigit() and len(cpf) == 11:
+                return True
+            else:
+                raise ValueError("O CPF deve conter apenas numeros e 11 algorismos")
+        else:
+            raise ValueError("O Cpf não pode ser vazio!")
+    except Exception as e:
+        print(e)
+    return False
+    
+def validarRg(rg:str) -> bool:  
+    try:
+        rg = rg.strip()
+        if len(rg) > 0:
+            if ".-" in rg:
+                rg = rg.replace(".", "")
+                rg = rg.replace("-", "")
+            if rg.isdigit() and len(rg) == 9:
+                return True
+            else:
+                raise Exception("O RG deve conter apenas numeros e 9 algorismos")
+        else:
+            raise ValueError("O Rg não pode ser vazio!")
+    except ValueError:
+        print("Valor invalido!")
+    except Exception as e:
+        print(e)
+    return False
 
 def validarLogin(login:str, opcao:str) -> bool:
     try:
         match opcao:
             case "1": #email
-                if len(login) > 0:
-                    login_separado = login.split("@")
-                    dominio_separado = login_separado[1].split(".")
-                    if len(login_separado) == 2 or len(dominio_separado) in [2,3]:
-                        for dominio, login in dominio_separado, login_separado:
-                            if len(dominio) == 2 or len(login) == 2 :
-                                return True
-                            else:
-                                raise ValueError("Email invalido!")
-                    else:
-                        raise ValueError("Email invalido!")
-                else:
-                    raise ValueError("O email não pode ser vazio!")
-                return False
-            
+                return validarEmail(login)
             case "2": #Usuario
-                if len(login) > 0:
-                    for caracter in login:
-                        if "_" in caracter:
-                            caracter = caracter.replace("_", "")
-                        if caracter.isalnum():
-                            return True
-                        else:
-                            raise ValueError("Usuario pode conter apenas numeros letras e _")
-                else:
-                    raise ValueError("O valor do usuario não pode ser vazio")
-                return False
-            
+                return validarUsuario(login)
             case "3": #Cpf
-                if len(login) > 0:
-                    if ".-" in login:
-                        login = login.replace(".", "")
-                        login = login.replace("-", "")
-
-                    if login.isdigit() and len(login) == 11:
-                        return True
-                    else:
-                        raise ValueError("O CPF deve conter apenas numeros e 11 algorismos")
-                else:
-                    raise ValueError("O Cpf não pode ser vazio!")
-                return False
-            
+                return validarCpf(login)
             case "4": #Rg
-                if len(login) > 0:
-                    if ".-" in login:
-                        login = login.replace(".", "")
-                        login = login.replace("-", "")
-                    if login.isdigit() and len(login) == 9:
-                        return True
-                    else:
-                        raise ValueError("O RG deve conter apenas numeros e 9 algorismos")
-                else:
-                    raise ValueError("O Rg não pode ser vazio!")
-                return False
+                return validarRg(login)
+    except ValueError:
+        print("Valor invalido!")
     except Exception as e:
         print(e)
+    return False
+
+def validarNome(nome:str) -> bool:
+    try:
+        nome = nome.strip()
+        if len(nome) > 0:
+            return nome.isalpha()
+        else:
+            raise Exception("O Nome não pode estar vazio!")
+    except ValueError:
+        print("Valor invalido!")
+    except Exception as e:
+        print(e)
+    return False
+
+def _validarData(dia:int, mes:int, ano:int,):
+    try:
+        dataAtual = datetime.datetime.now().date()
+        verificarDia = dia > 0 and dia <= 31
+        verificarMes = mes > 0 and mes <= 12
+        verificarAno = ano > 1600 and ano <= dataAtual.year
+
+        if verificarAno and ano == dataAtual.year:
+            if verificarDia and dia <= dataAtual.day or verificarMes and mes <= dataAtual.month:
+                return True
+            else:
+                raise Exception("O mês/dia não pode ser maior que o mes/dia atual!")
+        elif verificarAno:
+            if verificarMes:
+                if verificarDia:
+                    return True
+                else: 
+                   raise Exception("O Dia deve ser maior que 0 e menor ou igual a 31")
+            else:
+                raise Exception("O Mês deve ser maior que 0 e menor ou igual a 12")
+        else:
+            raise Exception("Ano invalido")
+    except ValueError:
+        print("Valor invalido")
+    except Exception as e:
+        print(e)
+    return False
 
 def validarData(data:str) -> bool:
-    dataAtual = datetime.datetime.now().date()
-    ano = dataAtual.year
-    mes = dataAtual.month
-    dia = dataAtual.day
+    try:
+        data = data.strip()
+        dataAtual = datetime.datetime.now().date()
+        if "/" in data:
+            if data.count("/") == 2: 
+                data_separada = data.split("/")
+                ano = data_separada[2]
+                mes = data_separada[1]
+                dia = data_separada[0]
+                if len(dia) == 2 and len(mes) == 2 and dia.isdigit() and mes.isdigit():
+                    if len(ano) == 4 and ano.isdigit() and int(ano) <= dataAtual.year:
+                        return _validarData(int(dia), int(mes), int(ano))
+                    else:
+                        raise Exception("Ano invalido!")
+                else:
+                    raise Exception("Dia ou mes invalido!")
+            else:
+                raise Exception("Formato de data invalido, O formato deve ser XX/XX/XXXX")
+        
+        else:
+            raise Exception("Formato de data invalido, O formato deve ser XX/XX/XXXX")
+        
+    except ValueError:
+        print("Valor invalido!")
+    except Exception as e:
+        print(e)
+    return False
 
-    data_separada = data.split("/")
-    contador = 0
-    for data in data_separada:
-        if len(data) != 2:
-            return False
-        contador += 1
-    if contador != 3:
-        return False
-    else:
-        if data_separada[0] > dia and data_separada[1] and data_separada[2] > ano:
-            return False
-    return True
+def validarEndereco(endereco:str) -> bool:
+    try:
+        endereco = endereco.strip()
+        if len(endereco) <= 0:
+            raise Exception("O endereço não pode estar vazio!")
+        else:
+            if "-" in endereco or "," in endereco:
+                endereco = endereco.replace(",","")
+                endereco = endereco.replace("-", "")
+            if endereco.isalnum():
+                return True
+            else: 
+                raise Exception("Só é permitido conter numeros, letras e ',-' no endereço!")
+    except ValueError:
+        print("Valor invalido!")
+    except Exception as e:
+        print(e)
+    return False
+
+def confimarSaida(resposta:str) -> bool:
+    try:
+        if resposta.title().strip() in ["Sim", "Não", "Nao"]:
+            return True
+        else:
+            raise Exception("Digite apenas com [Sim/Não]!")
+    except Exception as e:
+        print(e)
+    return False
+
+def verificarCampo(campo:str, listaCampos:list[str]) -> bool:
+    try:
+        if campo.isdigit():
+            if len(campo) < 0:
+                raise Exception("Valor do campo não pode ser vazio!")
+            else:
+                if int(campo) in range(10):
+                    if campo not in listaCampos:
+                        return True
+                    else:
+                        raise Exception("Campo ja adicionado!")
+                else:
+                    raise Exception("Campo invalido!")
+        else:
+            raise Exception("O campo precisar ser um dos indices informados!")
+    except ValueError:
+        print("Valor invalido!")
+    except Exception as e:
+        print(e)
+    return False
+
+def validarRole(role:str) -> bool:
+    try:
+        if role.strip().lower() in ["admin", "user"]:
+            return role.strip().isalpha()
+        else:
+            raise Exception("Role pode ser apenas admin ou user")
+    except Exception as e:
+        print(e)
+    return False
